ad325adaa80b965ffb1a9fc296fad9a505be4bd8
Chriswilliams231 <70972542+Chriswilliams231@users.noreply.github.com>
2020-12-10 17:10:26 -0800

    components

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,20 +1,25 @@
 import React from "react";
 import axios from "axios";
 
+import { MovieCard } from "../movie-card/movie-card";
+import { MovieView } from "../movie-view/movie-view";
+
 export class MainView extends React.Component {
   constructor() {
     // Call the superclass constructor
     // so React can initialize it
     super();
 
-    // Initialize the state to an empty object so we can destructure it later
-    this.state = {};
+    this.state = {
+      movies: null,
+      selectedMovie: null,
+    };
   }
 
   // One of the "hooks" available in a React Component
   componentDidMount() {
     axios
-      .get("<https://myflix89.herokuapp.com/movies>")
+      .get("https://myflix89.herokuapp.com/movies")
       .then((response) => {
         // Assign the result to the state
         this.setState({
@@ -26,21 +31,31 @@ export class MainView extends React.Component {
       });
   }
 
+  onMovieClick(movie) {
+    this.setState({
+      selectedMovie: movie,
+    });
+  }
+
   render() {
-    // If the state isn't initialized, this will throw on runtime
-    // before the data is initially loaded
-    const { movies } = this.state;
+    const { movies, selectedMovie } = this.state;
 
     // Before the movies have been loaded
     if (!movies) return <div className="main-view" />;
 
     return (
       <div className="main-view">
-        {movies.map((movie) => (
-          <div className="movie-card" key={movie._id}>
-            {movie.Title}
-          </div>
-        ))}
+        {selectedMovie ? (
+          <MovieView movie={selectedMovie} />
+        ) : (
+          movies.map((movie) => (
+            <MovieCard
+              key={movie._id}
+              movie={movie}
+              onClick={(movie) => this.onMovieClick(movie)}
+            />
+          ))
+        )}
       </div>
     );
   }
